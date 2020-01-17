ea2a8665008bafc7bca5e77a5d09a42cd84cc527
Sean Yeoh <sean.yeoh@hotmail.com>
2020-01-17 11:44:27 +0800

    add pdf for purchase order service

--- lib/quickbooks/service/purchase_order.rb
+++ lib/quickbooks/service/purchase_order.rb
@@ -6,6 +6,12 @@ module Quickbooks
         delete_by_query_string(purchase_order)
       end
 
+      def pdf(purchase_order)
+        url = "#{url_for_resource(model::REST_RESOURCE)}/#{purchase_order.id}/pdf"
+        response = do_http_raw_get(url, {}, {'Accept' => 'application/pdf'})
+        response.plain_body
+      end
+
     private
 
       def model
